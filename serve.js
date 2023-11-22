438973f01cae67010a376ae1ec16798914a84fd4
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-22 14:19:19 -0300

    fetch(user) Adiciona o rota para criar user

--- serve.js
+++ serve.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.post('/user', (req, res) => {
+    res.json({message: 'usuario criado com sucesso'})
+})
+
 app.listen(port, () => {
     console.log('rodando o servidor')
 })
