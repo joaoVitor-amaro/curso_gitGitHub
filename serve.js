328fc4388186bdfaa6fe49a9de61781afbf4ef76
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-22 14:27:55 -0300

    refactor(user) - lógica de criação de usuário extraido para uma função

--- serve.js
+++ serve.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function cadastrarUser() {
+    return {user: 'jv', id: 1}
+}
 
 app.post('/user', (req, res) => {
-    res.json({message: 'usuario criado com sucesso'})
+    const usuario = cadastrarUser(req.body)
+    res.json(usuario)
 })
 
 app.listen(port, () => {
